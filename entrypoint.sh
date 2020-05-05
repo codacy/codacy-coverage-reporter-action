4f8ea684831d4c25981f0f8836a2c89ab79f9912
Francisco Duarte <francisco@codacy.com>
2020-05-05 09:01:35 +0100

    feature: Add report list as parameter (#14)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 export CODACY_PROJECT_TOKEN=$1
-bash <(curl -Ls https://coverage.codacy.com/get.sh)
+# comma separated list of report files
+report_list=$2
+
+IFS=','
+report_array=$report_list
+params=''
+for report in $report_array
+do
+    if [ ! -z "$report" ]
+    then
+        params="$params -r $report"
+    fi
+done
+
+bash <(curl -Ls https://coverage.codacy.com/get.sh) report $params --partial &&\
+bash <(curl -Ls https://coverage.codacy.com/get.sh) final
